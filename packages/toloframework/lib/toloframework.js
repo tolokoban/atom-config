3204ccc345273762943e92a1d8d765527c2444e3
Fabien Petitjean <fabien.petitjean@dlodh.com>
2017-05-09 08:24:13 +0200

    New snippet : that.

---
+++ packages/toloframework/lib/toloframework.js
@@ -0,0 +1,49 @@
+'use babel';
+
+import ToloframeworkView from './toloframework-view';
+import { CompositeDisposable } from 'atom';
+
+export default {
+
+  toloframeworkView: null,
+  modalPanel: null,
+  subscriptions: null,
+
+  activate(state) {
+    this.toloframeworkView = new ToloframeworkView(state.toloframeworkViewState);
+    this.modalPanel = atom.workspace.addModalPanel({
+      item: this.toloframeworkView.getElement(),
+      visible: false
+    });
+
+    // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
+    this.subscriptions = new CompositeDisposable();
+
+    // Register command that toggles this view
+    this.subscriptions.add(atom.commands.add('atom-workspace', {
+      'toloframework:toggle': () => this.toggle()
+    }));
+  },
+
+  deactivate() {
+    this.modalPanel.destroy();
+    this.subscriptions.dispose();
+    this.toloframeworkView.destroy();
+  },
+
+  serialize() {
+    return {
+      toloframeworkViewState: this.toloframeworkView.serialize()
+    };
+  },
+
+  toggle() {
+    console.log('Toloframework was toggled!');
+    return (
+      this.modalPanel.isVisible() ?
+      this.modalPanel.hide() :
+      this.modalPanel.show()
+    );
+  }
+
+};
---
+++ packages/toloframework/spec/toloframework-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ToloframeworkView from '../lib/toloframework-view';
+
+describe('ToloframeworkView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
