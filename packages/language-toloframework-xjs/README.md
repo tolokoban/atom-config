ed14484c7095101a1d5e70ed24ea305e9a1ba46c
Tolokoban <fabien.petitjean@free.fr>
2018-10-06 14:34:50 +0200

    Removed packages because they are different between Linux and Window, and because they take a lot of disk space.

--- packages/language-toloframework-xjs/README.md
+++
@@ -1,2 +0,0 @@
-# language-toloframework-xjs
-XJS grammar for ATOM
--- packages/toloframework/spec/toloframework-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ToloframeworkView from '../lib/toloframework-view';
-
-describe('ToloframeworkView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
