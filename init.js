eafe4b60db16623d2a3d8b28a436b05e59f01190
Fabien Petitjean <f.petitjean@lombardodier.com>
2018-10-18 17:25:52 +0200

    Toloframework functions.

--- init.js
+++ init.js
@@ -1,30 +1,56 @@
 "use strict";
 
-atom.commands.add( 'atom-text-editor', 'toloframework:switch-to-xjs', switchToXJS );
+const Path = require( "path" );
 
-function switchToXJS() {
+['js', 'xjs', 'ini', 'css', 'vert', 'frag', 'dep'].forEach(function (extension) {
+  atom.commands.add(
+    'atom-text-editor',
+    'toloframework:switch-to-' + extension,
+    switchTo.bind( null, extension ) );
+});
+
+atom.commands.add(
+  'atom-text-editor',
+  'toloframework:switch-to-spec',
+  function() {
     const filename = getCurrentEditorPath();
-    if ( !filename ) return;
-    atom.workspace.open( replaceExtension( filename, ".xjs" ) );
-    const editor = atom.workspace.getActiveTextEditor();
-    if ( !editor.isEmpty() ) return;
-    const buffer = editor.getBuffer();
-    
+    const parsing = Path.parse( filename );
+    const dst = Path.join(
+      parsing.dir,
+      "..",
+      "..",
+      "spec",
+      replaceExtension( parsing.base, ".spec.js" )
+    );
+    atom.workspace.open( dst );
+  });
+
+
+function switchTo( extension ) {
+  const filename = getCurrentEditorPath();
+  if ( !filename ) return;
+  atom.workspace.open( replaceExtension( filename, extension ) );
 }
 
 function getCurrentEditorPath() {
-    const editor = atom.workspace.getActiveTextEditor();
-    if ( !editor ) return null;
-    return editor.getPath();
+  const editor = atom.workspace.getActiveTextEditor();
+  if ( !editor ) return null;
+  return editor.getPath();
 }
 
 function replaceExtension( filename, newExtension ) {
-    const pos = filename.lastIndexOf( '.' );
-    if ( newExtension.startsWith( "." ) ) {
-        if ( pos < 0 ) return filename + newExtension;
-        return filename.substr( 0, pos ) + newExtension;
-    } else {
-        if ( pos < 0 ) return filename + "." + newExtension;
-        return filename.substr( 0, pos ) + "." + newExtension;
-    }
+  const pos = filename.lastIndexOf( '.' );
+  if ( newExtension.startsWith( "." ) ) {
+    if ( pos < 0 ) return filename + newExtension;
+    return filename.substr( 0, pos ) + newExtension;
+  } else {
+    if ( pos < 0 ) return filename + "." + newExtension;
+    return filename.substr( 0, pos ) + "." + newExtension;
+  }
+}
+
+function hyphenate( filename ) {
+  const name = Path.parse( filename ).name;
+  const parts = name.split( /[^a-zA-Z0-9]+/ );
+  return parts.map( ( s ) => s.toLowercase() ).join( '-' );
 }
